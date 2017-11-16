dd5c6aeaefe14f36f674e1d4ea25d4609f7b21ff
tkellehe <mitchell.kelleher@gmail.com>
2017-11-16 18:04:03 -0500

    Create math_ops.js

---
+++ src/math_ops.js
@@ -0,0 +1,22 @@
+(function(global, characters, Parser, stdin, stdout, NUMBER, STRING, ARRAY, Reference){
+
+
+//----------------------------------------------------------------------------------------
+Parser.tokens[characters.handle_bug("+")] = {
+    tokenize : function(parser, method, iter) {
+        method.add({
+            exec : function(parser, context) {
+                var a = parser.next_param().get();
+                var b = parser.next_param().get();
+                var r = parser.next_param();
+                r.set(a.add_item_onto_the_right(b));
+                return context.next();
+            }
+        });
+
+        return iter.next();
+    }
+}
+
+
+})(this, this.characters, this.Parser, this.stdin, this.stdout, this.NUMBER, this.STRING, this.ARRAY, this.Reference);
